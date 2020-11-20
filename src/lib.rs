7e9b4a76570f9781d79a11d83d962b339b89b676
yi lu <luyikk@126.com>
2020-11-20 12:02:31 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,24 @@ pub mod queue;
 pub use queue::{AQueueItem,AQueue,QueueItem};
 
 pub use aqueue_trait::async_trait as aqueue_trait;
+use std::cell::UnsafeCell;
 
+pub struct InnerStore<T>(UnsafeCell<T>);
+unsafe impl<T> Sync for InnerStore<T>{}
+unsafe impl<T> Send for InnerStore<T>{}
+impl<T> InnerStore<T>{
+    pub fn new(x:T)-> InnerStore<T>{
+        InnerStore(UnsafeCell::new(x))
+    }
+    pub fn get_mut(&self)->&mut T{
+        unsafe{
+            &mut *self.0.get()
+        }
+    }
+
+    pub fn get(&self)->&T{
+        unsafe{
+            &*self.0.get()
+        }
+    }
+}
