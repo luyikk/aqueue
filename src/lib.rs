976faba75570608c8be6cf1661e76fb76f00633c
luyi <luyikk@126.com>
2023-01-30 14:32:44 +0800

    add timeout inner call

--- src/lib.rs
+++ src/lib.rs
@@ -10,28 +10,36 @@ pub use queue::AQueue;
 /// ``` toml
 /// aqueue = { version = "^1.2.6", features = ["wait_tokio"] }
 /// ```
-/// async_std:
-/// ``` toml
-/// aqueue = { version = "^1.2.6", features = ["wait_async_std"] }
+/// # Example
+/// ``` doc
+///     async fn test_unsafe_blocking(&self, name: String, gold: f64) -> anyhow::Result<bool> {
+///         inner_wait!(self, 30000, |_| async move { DB.insert_user(name, gold).await }).await?
+///     }
 /// ```
-#[doc = r#"
-# Example
-```doc
-    async fn test_unsafe_blocking(&self, name: String, gold: f64) -> anyhow::Result<bool> {
-        inner_wait!(self, 30000, |_| async move { DB.insert_user(name, gold).await }).await?
-    }
-```
-"#]
+#[cfg(all(feature = "wait_tokio", not(feature = "wait_async_std")))]
 #[macro_export]
 macro_rules! inner_wait {
     ($actor:expr,$timeout:expr,$fun:expr) => {{
-        #[cfg(feature = "wait_tokio")]
-        {
-            tokio::time::timeout(std::time::Duration::from_millis($timeout), $actor.inner_call($fun))
-        }
-        #[cfg(feature = "wait_async_std")]
-        {
-            async_std::future::timeout(std::time::Duration::from_millis($timeout), $actor.inner_call($fun))
-        }
+        tokio::time::timeout(std::time::Duration::from_millis($timeout), $actor.inner_call($fun))
+    }};
+}
+
+/// inner call wait ms throw time error
+/// need on feature "wait_tokio" or "wait_async_std"
+/// tokio:
+/// ``` toml
+/// aqueue = { version = "^1.2.6", features = ["wait_async_std"] }
+/// ```
+/// # Example
+/// ``` doc
+///     async fn test_unsafe_blocking(&self, name: String, gold: f64) -> anyhow::Result<bool> {
+///         inner_wait!(self, 30000, |_| async move { DB.insert_user(name, gold).await }).await?
+///     }
+/// ```
+#[cfg(all(feature = "wait_async_std", not(feature = "wait_tokio")))]
+#[macro_export]
+macro_rules! inner_wait {
+    ($actor:expr,$timeout:expr,$fun:expr) => {{
+        async_std::future::timeout(std::time::Duration::from_millis($timeout), $actor.inner_call($fun))
     }};
 }
