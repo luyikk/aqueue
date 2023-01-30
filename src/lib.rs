7044dec559e87550b94bc110b0dc95e35eceae3b
luyi <luyikk@126.com>
2023-01-30 17:07:35 +0800

    add timeout inner call

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,18 @@ pub use actor::Actor;
 pub use queue::AQueue;
 
 /// inner call wait ms throw time error
-/// need on feature "wait_tokio" or "wait_async_std"
-/// tokio:
+/// need on feature "tokio" or "async_std"
+/// # tokio:
 /// ``` toml
-/// aqueue = { version = "^1.2.6", features = ["wait_tokio"] }
+/// aqueue = { version = "^1.2.6", features = ["tokio"] }
 /// ```
 /// # Example
-/// ``` doc
-///     async fn test_unsafe_blocking(&self, name: String, gold: f64) -> anyhow::Result<bool> {
+/// ``` ignore
+///     async fn test_unsafe_blocking(&self, name: String, gold: f64) -> Result<bool> {
 ///         inner_wait!(self, 30000, |_| async move { DB.insert_user(name, gold).await }).await?
 ///     }
 /// ```
-#[cfg(all(feature = "wait_tokio", not(feature = "wait_async_std")))]
+#[cfg(all(feature = "tokio", not(feature = "async_std")))]
 #[macro_export]
 macro_rules! inner_wait {
     ($actor:expr,$timeout:expr,$fun:expr) => {{
@@ -25,18 +25,18 @@ macro_rules! inner_wait {
 }
 
 /// inner call wait ms throw time error
-/// need on feature "wait_tokio" or "wait_async_std"
-/// tokio:
+/// need on feature "tokio" or "async_std"
+/// # async_std:
 /// ``` toml
-/// aqueue = { version = "^1.2.6", features = ["wait_async_std"] }
+/// aqueue = { version = "^1.2.6", features = ["async_std"] }
 /// ```
 /// # Example
-/// ``` doc
-///     async fn test_unsafe_blocking(&self, name: String, gold: f64) -> anyhow::Result<bool> {
+/// ``` ignore
+///     async fn test_unsafe_blocking(&self, name: String, gold: f64) -> Result<bool> {
 ///         inner_wait!(self, 30000, |_| async move { DB.insert_user(name, gold).await }).await?
 ///     }
 /// ```
-#[cfg(all(feature = "wait_async_std", not(feature = "wait_tokio")))]
+#[cfg(all(feature = "async_std", not(feature = "tokio")))]
 #[macro_export]
 macro_rules! inner_wait {
     ($actor:expr,$timeout:expr,$fun:expr) => {{
